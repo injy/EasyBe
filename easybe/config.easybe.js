d2707c4517a6a56177afcbadcce58f9bd319e49d
injy <injy@qq.com>
2023-08-06 13:07:26 +0800

    Create config.easybe.js
    SAVE CONFIG AS FILE

---
+++ easybe/config.easybe.js
@@ -0,0 +1,286 @@
+    window.cnblogsConfig = {
+        info: {
+            name: 'kc', 
+            position: "China",
+            proverb:"good good study, day day up!",
+            connect: "http://wpa.qq.com/msgrd?v=3&uin=287653987&site=qq&menu=yes",
+            startDate: '2021-01-01', 
+            avatar: 'https://q1.qlogo.cn/g?b=qq&nk=287653987&s=640', 
+            // 用户头像
+            blogIcon: "https://q1.qlogo.cn/g?b=qq&nk=287653987&s=640"
+        },
+        banner: {
+            home: {
+                background: [
+                    "https://cn.bing.com/th?id=OHR.AtlanticPuffin_ZH-CN8523220989_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+                    "https://cn.bing.com/th?id=OHR.GothicRuins_ZH-CN8317467997_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+                    "https://cn.bing.com/th?id=OHR.ZelenciSprings_ZH-CN8022746409_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+                    "https://cn.bing.com/th?id=OHR.CapitolButte_ZH-CN7707972988_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+                    "https://cn.bing.com/th?id=OHR.DenaliClimber_ZH-CN7548168932_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+                    "https://cn.bing.com/th?id=OHR.RockHouse_ZH-CN7318310409_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+                    "https://cn.bing.com/th?id=OHR.PalouseHills_ZH-CN6864015897_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+                    "https://cn.bing.com/th?id=OHR.TigerIndia_ZH-CN6657629375_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp"
+                ]
+            }
+        },
+        sidebar: {
+            titleMsg: "欢迎使用创意基地开放邮箱系统~",
+            blogStatus: false,  //显示评论条数等
+            infoBackground:"https://images.cnblogs.com/cnblogs_com/wangyang0210/1943283/o_220918180339_wallhaven-g7og6e_522x929.webp",    // 列表数据 ['导航名称', '链接', 'icon']
+            navList: [
+              ["Home", "http://e.hm/",  "icon-brush-fill"],
+              ["News", "https://e.hm/gonggao", "icon-github"],
+              ["Service", "https://e.hm/my", "icon-cnblogs"],
+              ["FAQ","https://e.hm/faq",  "icon-book"],
+              [ "Links",   "https://e.hm/links",  "icon-lianjie"]
+            ],
+            customList: {
+              自定义列表1: {
+                // 标题
+                data: [
+                  // 列表数据 ['列表', '链接']
+                  ["我的博客1", "#1"],
+                  ["我的博客2", "#2"],
+                  ["我的博客5", "#3"]
+                ],
+                icon: "icon-brush-fill" // 配置图标，参考文档：定制化/字体图标库
+              },
+              自定义列表2: {
+                // 标题
+                data: [
+                  // 列表数据 ['列表', '链接']
+                  ["我的博客6", "#11"],
+                  ["我的博客7", "#12"],
+                  ["我的博客10", "#13"]
+                ],
+                icon: "icon-brush-fill" // 配置图标，参考文档：定制化/字体图标库
+              }
+            }
+        },
+        banner: {
+            home: {
+//                title: [
+//                    "白日依山尽，黄河入海流。<br />欲穷千里目，更上一层楼。 ",
+//                    "中国好啊！"
+//                    ],
+                titleSource: "jinrishici",
+                background: [
+                    "https://cn.bing.com/th?id=OHR.ZelenciSprings_ZH-CN8022746409_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+                    "https://cn.bing.com/th?id=OHR.GothicRuins_ZH-CN8317467997_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp"
+                ]
+            },
+            article: {
+              background: [
+                "https://cn.bing.com/th?id=OHR.GothicRuins_ZH-CN8317467997_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp"
+              ]
+            }
+        },
+        loading: {
+            rebound: {
+              tension: 16,
+            },
+            spinner: {
+              id: "spinner",
+              radius: 90,
+            }
+        },
+        title: {
+            focus: "(*´∇｀*) 欢迎回来！",
+            focusTime: 1000,
+            onblur: "(oﾟvﾟ)ノ Hi",
+            onblurTime: 500
+        },
+        footer: {
+            style: 2,
+            text: {
+                left: "学无止境",
+                right: "谦卑而行",
+                iconFont: {
+                    icon: "icon-xinlv",
+                    color: "red",
+                    fontSize: "16px"
+                }
+            },
+            aplayer: {
+                enable: true,
+                options: {
+                    id: "3778678",
+                    server: "netease",
+                    type: "playlist",
+                    auto: "netease",
+                    fixed: "true",
+                    mini: "true",
+                    autoplay: "false",
+                    theme: "#2980b9",
+                    loop: "all",
+                    order: "random",
+                    preload: "auto",
+                    volume: "0.7",
+                    mutex: "true",
+                    lrcType: "0",
+                    listFolded: "true",
+                    listMaxHeight: "340px",
+                    storageHame: "metingjs",
+                }
+            }
+        },
+        links: {
+            footer: [
+                  ["ab.cd", "https://ab.cd"],
+                  ["申请坑位", "#"],
+                  ["申请坑位", "#"],
+                  ["申请坑位", "#"],
+                  ["申请坑位", "#"],
+            ],
+            page: [
+                {
+                    title: "友情链接", // 标题
+                    icon: "icon-lianjie", // iconfont
+                    style: "color: #a78bfa;",
+                    links: [
+                          {
+                            name: "昵称", // 昵称
+                            introduction: "IT技术类博客", // 简介
+                            avatar:
+                              "https://pic3.zhimg.com/v2-f4cacab46386a7fb655c8405d7f9bedf_720w.webp?source=d16d100b", // 头像
+                            url: "#", // 友链地址
+                          },
+                          {
+                            name: "昵称", // 昵称
+                            introduction: "IT技术类博客", // 简介
+                            avatar: "#", // 头像
+                            url: "#", // 友链地址
+                          }
+                    ]
+                }
+            ]
+        },
+        rtMenu: {
+            qrCode: "#url",
+            reward: {
+              alipay: "url",
+              wechatpay: "url.jpg",
+            },
+            downScrollDom: "#blog_post_info_block",
+        },
+        switchDayNight: {
+            enable: true,
+            auto: {
+              enable: true,
+            }
+        },
+        animate: {
+            homeBanner: {
+              enable: true,
+              options: {
+                radius: 15
+              }
+            },
+            articleTitle: {
+              enable: true
+            },
+            articleBanner: {
+              enable: true
+            },
+            background: {
+                ribbons: {
+                    enable: false,
+                    options: {
+                      colorSaturation: "60%",
+                      colorBrightness: "50%",
+                      colorAlpha: 0.5,
+                      colorCycleSpeed: 5,
+                      verticalPosition: "random",
+                      horizontalSpeed: 200,
+                      ribbonCount: 3,
+                      strokeSize: 0,
+                      parallaxAmount: -0.2,
+                      animateSections: true
+                    }
+                },
+                season: {
+                    enable: false,
+                    options: {
+                        img: "data:image/png;base64,11111111",
+                        size: 40
+                    }
+                },
+                bannerImages: {
+                    enable: true
+                }
+            },
+            mouse: {
+                bubble: {
+                    enable: false,
+                    options: {
+                      live: 30,
+                      colors: ["149, 197, 252", "224, 199, 252"],
+                      quantity: 15,
+                      size: 5
+                    }
+                }
+            },
+            avatar: {
+                enable: true
+            }
+        },
+        code: {
+            type: "hljs",
+            options: {
+                hljs: {
+                    theme: "atom-one-dark-reasonable",
+                    languages: ["Bash", "CSS", "Dockerfile", "Go", "HTML", "XML", "HTTP"]
+                },
+                line: true,
+                macStyle: true
+            }
+        },
+        articleDirectory: {
+            position: "left"
+        },
+        articleContent: {
+            link: true,
+            owo: {
+                enable: false,
+                options: {
+                    Emoji: {
+                      type: "emoji",
+                      container: [
+                        {
+                          icon: "😀",
+                          text: ""
+                        }
+                      ]
+                    }
+                }
+            }
+        },
+/*        
+        articleSuffix: {
+            imgUrl: "url.jpg",
+            aboutHtml: "I am a good person",
+            copyrightHtml: "版权...",
+            copyText: {
+              enable: false,
+              length: 30,
+              copyright: "",
+            }, 
+            supportHtml: "声援..."
+        },
+*/
+        consoleList: [
+            ["console", "#url"],
+            ["console2", "#url"],
+            ["e Email", "#email"]
+        ],
+/*
+        beian: {
+            info: "#备案号"
+        },
+        gonganbeian: {
+            info: "备案号",
+            link: "#url"
+        },
+*/        
+        memorialDays: ["12-13", "07-27"]
+    }
